0871c604c5c62687180787f77dc09ee01fea9974
Reto Ryter <reto@twy.gmbh>
2020-09-18 10:28:37 +0200

    chore(testing): make it faster -.-
    chore(untrack): remove build folder

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFilesAfterEnv: ["./test/setupJest.ts"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
+  },
 };
